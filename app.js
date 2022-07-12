490796c9fb944555c444cddcf9a96138a062e5a4
zhy2on <dhwlgus5940@gmail.com>
2022-07-12 17:40:02 +0900

    Feat; get 연습

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express')
 var app = express()
 app.listen(3000, function() {
-	console.log("start!! express server on port 3000");
-}); // 비동기 callback 함수
+	console.log("start!! express server on port 3000")
+}) // 비동기 callback 함수
 
+app.use(express.static('public'))
+
+//url routing
 app.get('/', function(req, res) {
+	console.log('test')
 	res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/main', function(req,res) {
+	res.sendFile(__dirname + '/public/main.html')
 })
